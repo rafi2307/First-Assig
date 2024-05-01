08a4dc34080f48fd4bd11dd4d8b6f3a9c879e091
rafi2307 <rafiulislam1812@gmail.com>
2024-05-01 23:22:37 +0600

    i am rafi07

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Person x = new Person();
+        System.out.println(x.getName());
+        System.out.println(x.getAge());
+    }
+}
